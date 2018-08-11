3b1a69d9212511db0cf62ee2a4a29e737b3da04b
Nick Hebert <hebert.nick@gmail.com>
2018-08-11 11:16:55 -0500

    2nd Edit

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,4 @@
 
-// Set up our chart
 var svgWidth = 1000;
 var svgHeight = 700;
 var margin = { top: 30, right: 40, bottom: 100, left: 100 };
@@ -8,7 +7,6 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 
-// Create an SVG wrapper, append an svg that will hold our chart and shift the latter by left and top margins
 
 var svg = d3.select(".chart")
     .append("svg")
@@ -17,7 +15,7 @@ var svg = d3.select(".chart")
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-// append an svg group
+
 var chart = svg.append("g");
 
 d3.select(".chart").append("div").attr("class", "tooltip").style("opacity", 0);
@@ -29,36 +27,35 @@ d3.json(url, function (err, data) {
     // copy data into global dataset
     dataset = data
 
-    // Initialize tooltip
+    
     var toolTip = d3.tip()
         .attr("class", "tooltip")
         // Define position
         .offset([80, -60])
-        // The html() method allows us to mix JavaScript with HTML in the callback function
+        
         .html(function (data) {
             var state = data.geography;
             return state;
         });
 
-    // Create tooltip
+    
     chart.call(toolTip);
 
-    // define scale functions(range)
+    
     var xScale = d3.scaleLinear().range([0, width]);
     var yScale = d3.scaleLinear().range([height, 0]);
 
-    // define axis functions
+   
     var xAxis = d3.axisBottom().scale(xScale);
     var yAxis = d3.axisLeft().scale(yScale);
 
-    // these variables store the min and max values in a column in data.csv
+    
     var xMin;
     var xMax;
     var yMin;
     var yMax;
 
-    // create a function to identify min, max values of a column in data.csv which in turn
-    // assigns the results to the variables created above
+    
 
     function findMinAndMaxX(dataColumnX) {
         xMin = d3.min(dataset, function (d) { return d[dataColumnX] * 0.8 });
@@ -70,21 +67,21 @@ d3.json(url, function (err, data) {
         yMax = d3.max(dataset, function (d) { return d[dataColumnY] * 1.2 });
     };
 
-    // set the default x-axis
+    
     var defaultAxisLabelX = "percentBelowPoverty"
 
-    // set the default y-axis
+    
     var defaultAxisLabelY = "smokers"
 
-    // call the findMinAndMax() on the default X Axis
+    
     findMinAndMaxX(defaultAxisLabelX)
     findMinAndMaxY(defaultAxisLabelY)
 
-    // set the domain of the axes
+    
     xScale.domain([xMin, xMax]);
     yScale.domain([yMin, yMax])
 
-    // create chart
+    
     chart.selectAll("circle")
         .data(dataset)
         .enter()
@@ -107,7 +104,7 @@ d3.json(url, function (err, data) {
             toolTip.hide(d);
         })
 
-    // create state labels
+    
     chart.selectAll("text")
         .data(dataset)
         .enter()
@@ -125,31 +122,31 @@ d3.json(url, function (err, data) {
         .attr("text-anchor", "middle")
         .attr("class","stateText")
 
-        // display tooltip on click
+        
         .on("mouseover", function (d) {
             toolTip.show(d);
         })
-        // hide tooltip on mouseout
+        
         .on("mouseout", function (d, i) {
             toolTip.hide(d);
         })
 
 
-    // create x-axis
+   
     chart.append("g")
         .attr("class", "x-axis")
         .attr("transform", `translate(0,${height})`)
         .call(xAxis);
 
-    // create y-axis
+    
     chart.append("g")
         .attr("class", "y-axis")
         .call(yAxis)
 
 
-    // Append axes titles
+    
 
-    // add main x-axis title
+    
     chart.append("text")
         .attr("transform", `translate(${width - 40},${height - 5})`)
         .attr("class", "axis-text-main")
@@ -160,30 +157,30 @@ d3.json(url, function (err, data) {
         .attr("class", "axis-text-main")
         .text("Behavioral Risk Factors")
 
-    // add x-axis titles
+   
     chart.append("text")
         .attr("transform", `translate(${width / 2},${height + 40})`)
-        // This axis label is active by default
+        
         .attr("class", "axis-text-x active")
         .attr("data-axis-name", "percentBelowPoverty")
         .text("In Poverty (%)");
 
     chart.append("text")
         .attr("transform", `translate(${width / 2},${height + 60})`)
-        // This axis label is active by default
+        
         .attr("class", "axis-text-x inactive")
         .attr("data-axis-name", "medianIncome")
         .text("Household Income (Median)");
 
     chart.append("text")
         .attr("transform", `translate(${width / 2},${height + 80})`)
-        // This axis label is active by default
+        
         .attr("class", "axis-text-x inactive")
         .attr("data-axis-name", "ageDependencyRatio")
         .text("Age Dependency Ratio");
 
 
-    // add y-axis titles 
+    
     chart.append("text")
         .attr("transform", `translate(-40,${height / 2})rotate(270)`)
         .attr("class", "axis-text-y active")
@@ -204,7 +201,7 @@ d3.json(url, function (err, data) {
         .attr("data-axis-name", "physicallyActive")
         .text("Physically Active (%)");
 
-    // change the x axis's status from inactive to active when clicked and change all active to inactive
+    
     function labelChangeX(clickedAxis) {
         d3.selectAll(".axis-text-x")
             .filter(".active")
@@ -214,7 +211,7 @@ d3.json(url, function (err, data) {
         clickedAxis.classed("inactive", false).classed("active", true);
     }
 
-    // change the y axis's status from inactive to active when clicked and change all active to inactive
+    
     function labelChangeY(clickedAxis) {
         d3.selectAll(".axis-text-y")
             .filter(".active")
@@ -224,10 +221,10 @@ d3.json(url, function (err, data) {
         clickedAxis.classed("inactive", false).classed("active", true);
     }
 
-    // on click events for the x-axis
+    
     d3.selectAll(".axis-text-x").on("click", function () {
 
-        // assign the variable to the current axis
+        
         var clickedSelection = d3.select(this);
         var isClickedSelectionInactive = clickedSelection.classed("inactive");
         console.log("this axis is inactive", isClickedSelectionInactive)
@@ -241,7 +238,7 @@ d3.json(url, function (err, data) {
 
             xScale.domain([xMin, xMax]);
 
-            // create x-axis
+            
             svg.select(".x-axis")
                 .transition()
                 .duration(1000)
@@ -284,10 +281,10 @@ d3.json(url, function (err, data) {
         }
     });
 
-    // On click events for the y-axis
+    
     d3.selectAll(".axis-text-y").on("click", function () {
 
-        // assign the variable to the current axis
+        
         var clickedSelection = d3.select(this);
         var isClickedSelectionInactive = clickedSelection.classed("inactive");
         console.log("this axis is inactive", isClickedSelectionInactive)
@@ -301,7 +298,7 @@ d3.json(url, function (err, data) {
 
             yScale.domain([yMin, yMax]);
 
-            // create y-axis
+            
             svg.select(".y-axis")
                 .transition()
                 .duration(1000)
